e5df143dd7c11f8ad4164c7f070cc849f8bb1c50
dkeppel <Sam-Trathen@live.nmit.ac.nz>
2023-10-31 13:12:06 +1300

    Free login template

--- src/actions.js
+++ src/actions.js
@@ -26,3 +26,37 @@ const signIn = (email, password) => {
             }
         })
 };
+
+const signUp = (email, password, name) => {
+    firebaseApp
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(res => {
+        firebaseApp
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(res => {
+                firebaseApp
+                .database()
+                .ref(`user/${res.user.uid}`)
+                .set({
+                    email,
+                    name,
+                    id: res.user.uid
+                })
+                .then(() => {
+                    firebaseApp
+                    .database()
+                    .ref(`user/${res.user.uid}`)
+                    .on("value", data => {
+                        let user = {...data.val(), id: res.user.uid}
+                        user && localStorage.setItem("abcUser", JSON.stringify(user))
+                    })
+                })
+                .catch(err => {
+                    console.warn(err)
+                })
+            })
+
+    })
+}
